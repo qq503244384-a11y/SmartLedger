7494954e4324056a1e0273fbd58921556da2f4b8
qq503244384-a11y <qq503244384@gmail.com>
2025-12-12 16:57:41 +0800

    Upload SmartLedger project
    Upload SmartLedger project

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+}
+
+task("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+
+
+
+
+
